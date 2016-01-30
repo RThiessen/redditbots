c5d090b265f2bb0f41f4d1cfd137f1862273c885
Ronald Thiessen <ronald.t14@hotmail.com>
2016-01-29 23:49:25 -0600

    First run

---
+++ DeleteCommentBot/CommentDeleter.py
@@ -0,0 +1,45 @@
+#Ronald Thiessen
+import praw
+import time
+import getpass
+
+USERAGENT = ""
+#Description of bot for the API
+ID = ""
+SECRET = ""
+URI = ""
+REFRESH = ""
+
+print('Logging in...')
+
+r = praw.Reddit(USERAGENT)
+r.set_oauth_app_info(ID, SECRET, URI)
+r.refresh_access_information(REFRESH)
+
+
+def mainBot():
+    print('\nPlease enter a score threshold. All comments with a score less than the threshold will be deleted.')
+    THRESHOLD = int(input(''))
+    print('\nThis will delete all comments with score less than ' + str(THRESHOLD) + '.')
+    print('Are you sure you want to proceed? Y/N')
+    userInput = input('')
+    if userInput == 'y' or 'Y':
+        user = r.user
+        delete(user, THRESHOLD)
+
+def delete(user, THRESHOLD):
+    seconds = 3
+    print('\nFetching comments...')
+    comments = list(user.get_comments(limit=1000))
+    print('Processing ' + str(len(comments)) + ' comments.')
+    for item in comments:
+        if item.score < THRESHOLD:
+            print('Deleting ' + 'comment' + ' with score ' + str(item.score))
+            item.delete()
+
+def sleep(seconds):
+    time.sleep(seconds)
+
+while True:
+    mainBot()
+    
